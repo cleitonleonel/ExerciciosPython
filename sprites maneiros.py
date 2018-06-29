d67b8dfd799f4441d471362cc6b1b181c72c1631
Adenildo Junior <adenildojunior52@gmail.com>
2018-06-29 09:44:08 -0300

    adicionado arquivo do jogo. editado tv

--- sprites maneiros.py
+++ sprites maneiros.py
@@ -1,17 +1,21 @@
 def definir_sprite(direction,sequence):
-    direction = 3-1
-    sequence = 4-1
+    direction = direction
+    sequence = sequence
 
     largura = 128
     altura = 192
 
-    dividir_direcao = largura // 4 ** direction
-    dividir_sequencia = altura // 4 ** sequence
+    dividir_direcao = largura // 4
+    dividir_sequencia = altura // 4
 
-    direcao = direction * dividir_sequencia
-    sequencia = sequence * dividir_direcao
-    print(direcao)
-    print(sequencia)
+
+    direcao1 = dividir_direcao * (sequence-1)
+    sequencia1 = dividir_sequencia * (direction-1)
+    print(f'P1 = ({direcao1},{sequencia1})')
+
+    direcao2 = sequence * dividir_direcao
+    sequencia2 = direction * dividir_sequencia
+    print(f'P2 = ({direcao2},{sequencia2})')
 
     print('largura de um sprite:',dividir_direcao)
     print('altura de um sprite:',dividir_sequencia)
@@ -23,10 +27,10 @@ def definir_sprite(direction,sequence):
 7____|____|____|____
 6    |    |    |        
 5____|____|____|____
-4    |    |    |    
+4    |    |    |xxxx    
 3____|____|____|____
 2    |    |    |    
 1    |    |    |    
 '''
 
-definir_sprite(0,0)
+definir_sprite(2,4)
